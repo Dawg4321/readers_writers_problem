e3c26ff26ce75f4e2fc7ceaebaa1142c38f0c592
Dawg4321 <ryan.wiebe@outlook.com>
2021-12-13 11:51:50 +0000

    created src directory

--- build.sh
+++ build.sh
@@ -9,20 +9,28 @@ fi
 if [ ! -d "./build/writers_pref" ];then # ensuring writers preference build director exists
     mkdir -p build/writers_pref # making writers preference build directory 
 fi
+
+cd src # changing directory into source folder
+
 echo "Compiling readwrite.cpp..."
 g++ -c ./readwrite/readwrite.cpp -o temp # compiling readwrite.cpp
                                          # this will be linked to future compilations
 
 echo "Compiling and Linking Readers Preference..."
-g++ readers_pref/writer_readpref.cpp temp -o writer-r_pref # compiling readers preference
-g++ readers_pref/reader_readpref.cpp temp -o reader-r_pref
+g++ readers_pref/writer_readpref.cpp temp -o ../writer-r_pref # compiling readers preference
+g++ readers_pref/reader_readpref.cpp temp -o ../reader-r_pref
+
+echo "Compiling and Linking Writers Preference..."
+g++ writers_pref/writer_writerpref.cpp temp -o ../writer-w_pref # compiling writers preference
+g++ writers_pref/reader_writerpref.cpp temp -o ../reader-w_pref
+
+echo "Removing Excess Build files..." 
+rm temp # removing readwrite.cpp compilation files
+
+cd .. # changing to parent directory
 
 mv writer-r_pref ./build/readers_pref # moving executables to readers preference build folder
 mv reader-r_pref ./build/readers_pref
-
-echo "Compiling and Linking Writers Preference..."
-g++ writers_pref/writer_writerpref.cpp temp -o writer-w_pref # compiling writers preference
-g++ writers_pref/reader_writerpref.cpp temp -o reader-w_pref
 
 mv writer-w_pref ./build/writers_pref # moving executables to writers preference build folder
 mv reader-w_pref ./build/writers_pref
@@ -30,9 +38,6 @@ mv reader-w_pref ./build/writers_pref
 echo "Creating Text Files..."
 touch ./build/readers_pref/file.txt # creating text files for use by executables
 touch ./build/writers_pref/file.txt
-
-echo "Removing excess build files..." 
-rm temp # removing readwrite.cpp compilation files
 
 echo "Code Successfully Compiled and Ready to Run!"
 echo "Please run the executables created in /build to test code."

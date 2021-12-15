f2232a1b5a94f8d3a2875b9e111c980d588be071
Dawg4321 <ryan.wiebe@outlook.com>
2021-12-15 20:17:08 +0000

    improved modularity of build directory

--- build.sh
+++ build.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+dir="./build" # directory to build to
+
+reader_dir="${dir}/readers_pref" # sub directories for readers and writers preference
+writer_dir="${dir}/writers_pref"
+
 echo "Preparing to Compile Readers-Writers-Problem..."
 
-if [ ! -d "./build/readers_pref" ];then # ensuring readers preference build director exists
-    mkdir -p build/readers_pref # making readers preference build directory
+if [ ! -d $reader_dir  ];then # ensuring readers preference build director exists
+    mkdir -p $reader_dir # making readers preference build directory
+else
+    rm -- $reader_dir/* # removing any pre-existing files in build directory
 fi
 
-if [ ! -d "./build/writers_pref" ];then # ensuring writers preference build director exists
-    mkdir -p build/writers_pref # making writers preference build directory 
+if [ ! -d $writer_dir ];then # ensuring writers preference build director exists
+    mkdir -p $writer_dir # making writers preference build directory 
+else
+    rm -- $writer_dir/* # this ensures only the newly created files are in the build directory
 fi
-
-rm ./build/readers_pref/* # removing any existing files in build directory
-rm ./build/writers_pref/* # this ensures only the newly created files are in the build directory
 
 cd src # changing directory into source folder
 
@@ -20,23 +26,24 @@ g++ -c ./readwrite/readwrite.cpp -o temp # compiling readwrite.cpp
                                          # this will be linked to future compilations
 
 echo "Compiling and Linking Readers Preference..."
-g++ readers_pref/writer_readpref.cpp temp -o ../build/readers_pref/writer-r_pref # compiling readers preference code
-g++ readers_pref/reader_readpref.cpp temp -o ../build/readers_pref/reader-r_pref
-g++ readers_pref/initialization_readpref.cpp -o ../build/readers_pref/initialization-r_pref
+g++ readers_pref/writer_readpref.cpp temp -o ".$reader_dir/writer-r_pref" # compiling readers preference code
+g++ readers_pref/reader_readpref.cpp temp -o ".$reader_dir/reader-r_pref"
+g++ readers_pref/initialization_readpref.cpp -o ".$reader_dir/initialization-r_pref"
 
 echo "Compiling and Linking Writers Preference..."
-g++ writers_pref/writer_writerpref.cpp temp -o ../build/writers_pref/writer-w_pref # compiling writers preference
-g++ writers_pref/reader_writerpref.cpp temp -o ../build/writers_pref/reader-w_pref
-g++ writers_pref/initialization_writerpref.cpp -o ../build/writers_pref/initialization-w_pref
+g++ writers_pref/writer_writerpref.cpp temp -o ".$writer_dir/writer-w_pref" # compiling writers preference
+g++ writers_pref/reader_writerpref.cpp temp -o ".$writer_dir/reader-w_pref"
+g++ writers_pref/initialization_writerpref.cpp -o ".$writer_dir/initialization-w_pref"
 
 echo "Removing Excess Build files..." 
 rm temp # removing readwrite.cpp compilation files
 
-cd .. # changing to parent directory
+cd .. # changing to parent directory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
+
 
 echo "Creating Text Files..."
-touch ./build/readers_pref/file.txt # creating text files for use by executables
-touch ./build/writers_pref/file.txt 
+touch "$reader_dir/file.txt" # creating text files for use by executables
+touch "$writer_dir/file.txt" 
 
 echo "Code Successfully Compiled and Ready to Run!"
 echo "Please run the executables created in /build to test code."
